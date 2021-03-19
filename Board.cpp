a35726974ffe7cf2f8652553973182907d860151
Yuval Marmer <yuvalmarmer97@gmail.com>
2021-03-19 14:33:48 +0200

    Simple implemetes

---
+++ Board.cpp
@@ -0,0 +1,21 @@
+#include "Board.hpp"
+#include <string>
+#include "Direction.hpp"
+
+using namespace ariel;
+
+Board::Board(){
+	
+}
+
+
+void Board::post(int row, int column, Direction d, std::string message){
+	
+}
+std::string Board::read(int row, int column, Direction d , int length){
+	
+	return "";
+}
+
+void Board::show(){
+}
---
+++ Board.hpp
@@ -0,0 +1,13 @@
+#include <string>
+#include "Direction.hpp"
+#include <iostream>
+using namespace std;
+namespace ariel {
+    class Board{
+        public :
+			Board();
+			void post(int row, int column, Direction d, string message);
+			std::string read(int row, int column, Direction d, int length);
+			void show();
+    };
+}
