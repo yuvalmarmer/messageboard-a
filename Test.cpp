4e700103165b3d76fd14f5fbd47a9c71ee93296c
Yuval Marmer <yuvalmarmer97@gmail.com>
2021-03-19 19:19:46 +0200

    Adding test cases

--- Test.cpp
+++ Test.cpp
@@ -72,7 +72,7 @@ TEST_CASE("Simple Reading"){
         int col = rand()%100;
 
         //Posting the message
-        board->post(row, col, ariel::Direction::Horizontal, RandomString);
+        board->post(row, col, ariel::Direction::Horizontal, RandomString.c_str());
         //Checking read functinality works
         // CHECK(board->read(row, col, ariel::Direction::Horizontal, randomLength) == RandomString);
     }
